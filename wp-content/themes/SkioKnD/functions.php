7ae29f2e182eea7b4a463807f0fff6945b1aacf9
aalston004 <aalston004@gmail.com>
2015-12-17 15:26:07 -0500

    Slight start
    - que'd up bs.min
    - Added wp function to head

--- wp-content/themes/SkioKnD/functions.php
+++ wp-content/themes/SkioKnD/functions.php
@@ -118,11 +118,13 @@ function skio_knd_scripts() {
 	wp_enqueue_style( 'skio_knd-style', get_stylesheet_uri() );
 
 	//bs.css
-	wp_enqueue_style();
+	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css');
 
 	wp_enqueue_script( 'skio_knd-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'skio_knd-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
