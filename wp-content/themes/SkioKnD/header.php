5098cafbf941ec71f771cd7a274d4a5cbb35487d
aalston004 <aalston004@gmail.com>
2015-12-18 13:49:26 -0500

    Removed init menu

--- wp-content/themes/SkioKnD/header.php
+++ wp-content/themes/SkioKnD/header.php
@@ -33,9 +33,9 @@
       <div class="header clearfix">
         <nav>
           <ul class="nav nav-pills pull-right">
-            <li role="presentation" class="active"><a href="#">Home</a></li>
-            <li role="presentation"><a href="#">About</a></li>
-            <li role="presentation"><a href="#">Contact</a></li>
+            
+          	
+            
           </ul>
         </nav>
         <h3 class="text-muted"><?php bloginfo('name'); ?></h3>
