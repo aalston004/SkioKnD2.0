b7f8e86bfbbd85aaf9eec68e5e08dbdca9640b91
aalston004 <aalston004@gmail.com>
2015-12-21 11:59:46 -0500

    Updates to page
    - replaced content w/funct
    - added error statement to if statement

--- wp-content/themes/SkioKnD/page.php
+++ wp-content/themes/SkioKnD/page.php
@@ -3,14 +3,19 @@
      
       <div class="row marketing">
         <div class="col-lg-6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+          <?php if (have_posts()) : while (have_posts()) : the_post; ?>            
+            <div class="page-header">
+              <h1><?php the_title(); ?></h1>
+            </div>
+            <?php the_content(); ?>
 
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+          <?php endwhile; else: ?>
+            <div class="page-header">
+              <h1>Error!</h1>
+            </div>
+            <p>No content available</p>
 
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+          <?php endif; ?>
         </div>
 
         <div class="col-lg-6">
