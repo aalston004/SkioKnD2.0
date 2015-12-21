e8f58e668fb558158c25fde11ae791a16169a495
aalston004 <aalston004@gmail.com>
2015-12-20 22:14:06 -0500

    Front page
    - added front
    - tweaks to jumbo with a few functs

--- wp-content/themes/SkioKnD/front-page.php
+++ wp-content/themes/SkioKnD/front-page.php
@@ -1,24 +1,10 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package Skio_KnD
- * narrow jumbo
- */
-
 <?php get_header(); ?>
 
       <div class="jumbotron">
-        <h1>Jumbotron heading</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>        
+          <?php the_content(); ?>
+        <?php endwhile; endif; ?>
       </div>
 
       <div class="row marketing">
