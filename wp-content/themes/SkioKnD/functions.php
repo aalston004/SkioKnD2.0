179f1e5a08b803b265100dc055fba6ec44d09252
aalston004 <aalston004@gmail.com>
2015-12-22 11:51:03 -0500

    funct tweak
    bs enques were set to the same name

--- wp-content/themes/SkioKnD/functions.php
+++ wp-content/themes/SkioKnD/functions.php
@@ -149,7 +149,7 @@ function skio_knd_scripts() {
 	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css');
 
 	//bstheme.css
-	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/css/bootstrap-theme.min.css');
+	wp_enqueue_style( 'bootstrap_theme_css', get_template_directory_uri() . '/css/bootstrap-theme.min.css');
 
 	wp_enqueue_script( 'skio_knd-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
