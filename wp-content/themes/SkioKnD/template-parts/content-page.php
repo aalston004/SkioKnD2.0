404dc255184a56d89404d3d31b00d56975370851
aalston004 <aalston004@gmail.com>
2015-12-22 11:04:40 -0500

    Full width test12
    -testing if content page edits have any effect

--- wp-content/themes/SkioKnD/template-parts/content-page.php
+++ wp-content/themes/SkioKnD/template-parts/content-page.php
@@ -9,19 +9,8 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+<div class="page-header">
+	<h1><?php the_title(); ?></h1>
+</div>
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'skio_knd' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-</article><!-- #post-## -->
+<?php the_content(); ?>
