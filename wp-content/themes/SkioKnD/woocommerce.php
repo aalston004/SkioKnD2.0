d9d2de8a61c96beb35806f273119579ab77d48f9
aalston004 <aalston004@gmail.com>
2016-01-14 12:48:29 -0500

    Woo 3
    - previous commit successfully integrated woo
    - prod pages come out wonky, testing out different code for woo.php

--- wp-content/themes/SkioKnD/woocommerce.php
+++ wp-content/themes/SkioKnD/woocommerce.php
@@ -1,11 +1,13 @@
-function skio_knd_wrapper_start() {  
-    echo '&lt;div id="primary" class="content-area"&gt;';
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+add_action('woocommerce_before_main_content', 'skio_knd_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'skio_knd_wrapper_end', 10);
+
+function skio_knd_wrapper_start() {
+  echo '<section id="main">';
 }
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
-add_action( 'woocommerce_before_main_content', 'skio_knd_wrapper_start', 10 ); 
- 
+
 function skio_knd_wrapper_end() {
-    echo '&lt;/div&gt;';
+  echo '</section>';
 }
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );  
-add_action( 'woocommerce_after_main_content', 'skio_knd_wrapper_end', 10 );
